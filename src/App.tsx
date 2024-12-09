895e20ec3a9e6774240f486e2f968f262a8a7843
marimonon <mappi-@hotmail.co.jp>
2024-12-09 13:35:26 +0900

    3つの（シーンになる）空コンポーネント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import Container from "./components/Container"
+import Ending from "./components/Ending"
+import Escape from "./components/Escape"
+import Start from "./components/Start"
 
 function App() {
   return (
     <>
       <Container>
+        <Start />
+        <Escape />
+        <Ending />
         <h1>脱出ゲーム</h1>
         <button>スタート</button>
       </Container>
