05e9d28a61d788fd573ae20bd87df74312ca59bd
marimonon <mappi-@hotmail.co.jp>
2024-12-10 12:03:12 +0900

    矢印画像の追加

--- src/components/Arrows.tsx
+++ src/components/Arrows.tsx
@@ -1,5 +1,17 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+import ArrowImage from "./Images/ArrowImage"
+
 const Arrows: React.FC = () => {
-  return <div>{/* Add your component content here */}</div>
+  return (
+    <>
+      <div>
+        <ArrowImage />
+      </div>
+      <div>右</div>
+      <div>下</div>
+    </>
+  )
 }
 
 export default Arrows
