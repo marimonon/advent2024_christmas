6d9b9179b516f7103fbc9c6eb39c226861d9b09c
marimonon <mappi-@hotmail.co.jp>
2024-12-10 13:59:39 +0900

    snowmanBigのcss

--- src/components/Rooms/StoveRoom.tsx
+++ src/components/Rooms/StoveRoom.tsx
@@ -1,10 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
 import StoveRoomBg from "../Images/Stove/StoveRoomBg"
+import BigSnowmanImage from "../Images/Stove/BigSnowmanImage"
 
 const StoveRoom: React.FC = () => {
   return (
     <div>
+      <BigSnowmanImage css={bigSnowCss} />
       <StoveRoomBg css={stoveBgCss} />
     </div>
   )
@@ -12,6 +14,14 @@ const StoveRoom: React.FC = () => {
 
 export default StoveRoom
 
+const bigSnowCss = css`
+  position: absolute;
+  width: 54%;
+  height: auto;
+  top: 0%;
+  left: 5%;
+  z-index: 4;
+`
 const stoveBgCss = css`
   width: 100%;
   height: 100%;
