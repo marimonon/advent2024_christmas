a79bba1ffff705be92fc1ce68d5f6336466d8830
marimonon <mappi-@hotmail.co.jp>
2024-12-10 18:59:07 +0900

    小さい動物のcss

--- src/components/Rooms/WindowRoom.tsx
+++ src/components/Rooms/WindowRoom.tsx
@@ -3,10 +3,14 @@ import { css } from "@emotion/react"
 import WindowRoomBg from "../Images/Window/WindowRoomBg"
 import HungryDogImage from "../Images/Window/HungryDogImage"
 import WindowDeerImage from "../Images/Window/WindowDeerImage"
+import SweatDeerImage from "../Images/Window/SweatDeerImage"
+import EatingDogImage from "../Images/Window/EatingDogImage"
 
 const WindowRoom: React.FC = () => {
   return (
     <div>
+      <EatingDogImage css={eatingCss} />
+      <SweatDeerImage css={sweatCss} />
       <HungryDogImage css={hungryCss} />
       <WindowDeerImage css={windowDeerCss} />
       <WindowRoomBg css={windowBgCss} />
@@ -16,11 +20,29 @@ const WindowRoom: React.FC = () => {
 
 export default WindowRoom
 
+const eatingCss = css`
+  position: absolute;
+  top: 20%;
+  left: 62%;
+  width: 12%;
+  height: auto;
+  z-index: 4;
+`
+
+const sweatCss = css`
+  position: absolute;
+  top: 20%;
+  left: 62%;
+  width: 12%;
+  height: auto;
+  z-index: 1;
+`
+
 const hungryCss = css`
   position: absolute;
   top: 28%;
   left: 24%;
-  width: 40%;
+  width: 41%;
   height: auto;
   z-index: 3;
 `
