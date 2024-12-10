9119e9066a3b5032c316e98dfa195b61308bf864
marimonon <mappi-@hotmail.co.jp>
2024-12-10 15:49:28 +0900

    treeのライトのcss

--- src/components/Rooms/TreeRoom.tsx
+++ src/components/Rooms/TreeRoom.tsx
@@ -2,10 +2,14 @@
 import { css } from "@emotion/react"
 import TreeRoomBg from "../Images/Tree/TreeRoomBg"
 import TreeImage from "../Images/Tree/TreeImage"
+import TreeLightImage from "../Images/Tree/TreeLightImage"
+import TreeWhiteImage from "../Images/Tree/TreeWhiteImage"
 
 const TreeRoom: React.FC = () => {
   return (
     <div>
+      <TreeWhiteImage css={treeWhiteCss} />
+      <TreeLightImage css={treeLightCss} />
       <TreeImage css={treeCss} />
       <TreeRoomBg css={treeBgCss} />
     </div>
@@ -13,6 +17,24 @@ const TreeRoom: React.FC = () => {
 }
 
 export default TreeRoom
+
+const treeWhiteCss = css`
+  position: absolute;
+  width: 60%;
+  height: auto;
+  right: 8%;
+  bottom: 20%;
+  z-index: 3;
+`
+
+const treeLightCss = css`
+  position: absolute;
+  width: 64%;
+  height: auto;
+  right: 4%;
+  bottom: 16%;
+  z-index: 2;
+`
 
 const treeCss = css`
   position: absolute;
