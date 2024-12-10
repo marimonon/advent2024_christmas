1c6f1b778a9ccfe815be6b89a2f4af97925d19f5
marimonon <mappi-@hotmail.co.jp>
2024-12-10 13:44:10 +0900

    背景にCSS適用

--- src/components/Images/Stove/StoveRoomBg.tsx
+++ src/components/Images/Stove/StoveRoomBg.tsx
@@ -1,4 +1,7 @@
-const StoveRoomBg: React.FC = () => {
+type Props = {
+  className?: string
+}
+const StoveRoomBg: React.FC<Props> = ({ className }) => {
   return (
     <svg
       width="600"
@@ -6,6 +9,7 @@ const StoveRoomBg: React.FC = () => {
       viewBox="0 0 1000 1240"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className={className}
     >
       <g clip-path="url(#clip0_276_1110)">
         <rect width="1000" height="1240" fill="#045662" />
--- src/components/Rooms/StoveRoom.tsx
+++ src/components/Rooms/StoveRoom.tsx
@@ -5,9 +5,14 @@ import StoveRoomBg from "../Images/Stove/StoveRoomBg"
 const StoveRoom: React.FC = () => {
   return (
     <div>
-      <StoveRoomBg />
+      <StoveRoomBg css={stoveBgCss} />
     </div>
   )
 }
 
 export default StoveRoom
+
+const stoveBgCss = css`
+  width: 100%;
+  height: 100%;
+`
