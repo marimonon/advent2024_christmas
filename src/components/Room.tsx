af5564a23d9ec7bfb3bcdb9284c40a7ffba27c7b
marimonon <mappi-@hotmail.co.jp>
2024-12-10 11:52:32 +0900

    画面幅を実装

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,8 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+
 import React from "react"
 import StoveRoom from "./Rooms/StoveRoom"
 
 const Room: React.FC = () => {
-  return <StoveRoom />
+  return (
+    <div css={roomCss}>
+      <StoveRoom />
+    </div>
+  )
 }
 
 export default Room
+
+const roomCss = css`
+  width: 100vw;
+  maw-width: 600px;
+  height: 124vw;
+  max-height: 744px;
+`
