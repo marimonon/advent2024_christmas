342db821d8bcdd612ca6e6d4b85c4cf914812b44
marimonon <mappi-@hotmail.co.jp>
2024-12-10 21:42:15 +0900

    ツリーのスイッチのcssの追加

--- src/components/Rooms/ExTree.tsx
+++ src/components/Rooms/ExTree.tsx
@@ -1,13 +1,29 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
 import ExTreeBg from "../Images/Tree/ExTreeBg"
+import ExTreeSwitchImage from "../Images/Tree/ExTreeSwitchImage"
 
 const ExTree: React.FC = () => {
   return (
     <div>
-      <ExTreeBg />
+      <ExTreeSwitchImage css={ExSwitchCss} />
+      <ExTreeBg css={ExTreeCss} />
     </div>
   )
 }
 
 export default ExTree
+
+const ExSwitchCss = css`
+  position: absolute;
+  top: 68%;
+  left: 49%;
+  width: 8%;
+  height: auto;
+  z-index: 2;
+`
+
+const ExTreeCss = css`
+  width: 100%;
+  height: 100%;
+`
