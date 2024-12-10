bc6a63f048613c52aa6aebe9ad0700db0aad2170
marimonon <mappi-@hotmail.co.jp>
2024-12-10 20:24:42 +0900

    背景のcss

--- src/components/Rooms/ChildRoom.tsx
+++ src/components/Rooms/ChildRoom.tsx
@@ -1,7 +1,28 @@
-import React from "react"
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+import ChildRoomBg from "../Images/Child/ChildRoomBg"
+import HomeWorkImage from "../Images/Child/HomeWorkImage"
 
 const ChildRoom: React.FC = () => {
-  return <div>Child Room Component</div>
+  return (
+    <div>
+      <HomeWorkImage css={homeWorkCss} />
+      <ChildRoomBg css={childBgCss} />
+    </div>
+  )
 }
 
 export default ChildRoom
+
+const homeWorkCss = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 20%;
+  z-index: 2;
+`
+
+const childBgCss = css`
+  width: 100%;
+  height: 100%;
+`
