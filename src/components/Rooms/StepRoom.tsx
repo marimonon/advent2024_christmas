1862c232802f890565fdd395ad96fe1715bf4505
marimonon <mappi-@hotmail.co.jp>
2024-12-10 20:00:52 +0900

    背景のcss

--- src/components/Rooms/StepRoom.tsx
+++ src/components/Rooms/StepRoom.tsx
@@ -1,8 +1,18 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
+import StepRoomBg from "../Images/Step/StepRoomBg"
 
 const StepRoom: React.FC = () => {
-  return <div>{/* コンポーネントの内容をここに追加 */}</div>
+  return (
+    <div>
+      <StepRoomBg css={stepBgCss} />
+    </div>
+  )
 }
 
 export default StepRoom
+
+const stepBgCss = css`
+  width: 100%;
+  height: 100%;
+`
