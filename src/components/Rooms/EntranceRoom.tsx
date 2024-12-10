786354070f8a3ecaf42176762a88aee2a0372347
marimonon <mappi-@hotmail.co.jp>
2024-12-10 17:09:15 +0900

    bgのcssの追加

--- src/components/Rooms/EntranceRoom.tsx
+++ src/components/Rooms/EntranceRoom.tsx
@@ -1,11 +1,18 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
 import EntranceRoomBg from "../Images/Entrance/EntranceRoomBg"
 
 const EntranceRoom: React.FC = () => {
   return (
     <div>
-      <EntranceRoomBg />
+      <EntranceRoomBg css={entranceBg} />
     </div>
   )
 }
 
 export default EntranceRoom
+
+const entranceBg = css`
+  width: 100%;
+  height: 100%;
+`
