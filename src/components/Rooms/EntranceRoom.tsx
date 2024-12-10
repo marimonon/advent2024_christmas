e8de6909c8a802f55ed778634dcd04b993c2417c
marimonon <mappi-@hotmail.co.jp>
2024-12-10 17:10:48 +0900

    リボンのcssの追加

--- src/components/Rooms/EntranceRoom.tsx
+++ src/components/Rooms/EntranceRoom.tsx
@@ -1,16 +1,27 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
 import EntranceRoomBg from "../Images/Entrance/EntranceRoomBg"
+import RibbonImage from "../Images/Entrance/RibbonImage"
 
 const EntranceRoom: React.FC = () => {
   return (
     <div>
+      <RibbonImage css={ribbonCss} />
       <EntranceRoomBg css={entranceBg} />
     </div>
   )
 }
 
 export default EntranceRoom
+
+const ribbonCss = css`
+  position: absolute;
+  width: 30%;
+  height: auto;
+  bottom: 0%;
+  left: 8%;
+  z-index: 2;
+`
 
 const entranceBg = css`
   width: 100%;
