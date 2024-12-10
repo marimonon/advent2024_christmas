57534889a94452a60469f94cfdcdf94a351377e1
marimonon <mappi-@hotmail.co.jp>
2024-12-10 20:30:51 +0900

    寝てる子とbootのcss

--- src/components/Rooms/ChildRoom.tsx
+++ src/components/Rooms/ChildRoom.tsx
@@ -2,10 +2,14 @@
 import { css } from "@emotion/react"
 import ChildRoomBg from "../Images/Child/ChildRoomBg"
 import HomeWorkImage from "../Images/Child/HomeWorkImage"
+import ChildSleepImage from "../Images/Child/ChildSleepImage"
+import BootImage from "../Images/Child/BootImage"
 
 const ChildRoom: React.FC = () => {
   return (
     <div>
+      <BootImage css={bootCss} />
+      <ChildSleepImage css={childSleepCss} />
       <HomeWorkImage css={homeWorkCss} />
       <ChildRoomBg css={childBgCss} />
     </div>
@@ -14,11 +18,30 @@ const ChildRoom: React.FC = () => {
 
 export default ChildRoom
 
+const bootCss = css`
+  position: absolute;
+  top: 15%;
+  left: 47%;
+  width: 13%;
+  height: auto;
+  z-index: 1;
+`
+
+const childSleepCss = css`
+  position: absolute;
+  top: 26%;
+  left: 28%;
+  width: 76%;
+  height: auto;
+  z-index: 1;
+`
+
 const homeWorkCss = css`
   position: absolute;
-  top: 76%;
+  top: 80%;
   left: 36%;
   width: 40%;
+  height: auto;
   z-index: 2;
 `
 
