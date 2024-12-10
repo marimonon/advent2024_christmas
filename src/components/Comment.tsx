44d47f513a0ba84b3811379fd144a3c642035a16
marimonon <mappi-@hotmail.co.jp>
2024-12-11 07:51:56 +0900

    コメントのcss

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -7,10 +7,31 @@ type CommentProps = {
 
 const Comment: React.FC<CommentProps> = ({ children }) => {
   return (
-    <div>
-      <p>{children}</p>
+    <div css={commentCss}>
+      <p css={commentTextCss}>{children}</p>
     </div>
   )
 }
 
 export default Comment
+
+const commentCss = css`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10%;
+`
+
+const commentTextCss = css`
+  font-size: 16px;
+  color: #930606;
+  background: rgba(255, 255, 255, 0.9);
+  padding: 10%;
+  border-radius: 12px;
+`
