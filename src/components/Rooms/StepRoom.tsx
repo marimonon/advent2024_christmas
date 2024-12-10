59e777a4777c332a111d14461535ab8fc88c6552
marimonon <mappi-@hotmail.co.jp>
2024-12-10 19:59:04 +0900

    StepRoomのコンポーネントの作成

---
+++ src/components/Rooms/StepRoom.tsx
@@ -0,0 +1,8 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+
+const StepRoom: React.FC = () => {
+  return <div>{/* コンポーネントの内容をここに追加 */}</div>
+}
+
+export default StepRoom
