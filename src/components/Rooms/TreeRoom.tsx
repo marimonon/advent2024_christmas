f7679774f5b2e4c6fbb659d2f0a0d81b47edcf51
marimonon <mappi-@hotmail.co.jp>
2024-12-10 15:25:29 +0900

    treeコンポーネント空の作成

---
+++ src/components/Rooms/TreeRoom.tsx
@@ -0,0 +1,10 @@
+const TreeRoom: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Tree Room</h1>
+      <p>This is where the Christmas tree is located.</p>
+    </div>
+  )
+}
+
+export default TreeRoom
