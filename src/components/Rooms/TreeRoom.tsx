c5ba61f6da1b51537ff25e47ffc8af5cd48e3f24
marimonon <mappi-@hotmail.co.jp>
2024-12-10 15:44:56 +0900

    bgとtreeのcss

--- src/components/Rooms/TreeRoom.tsx
+++ src/components/Rooms/TreeRoom.tsx
@@ -1,10 +1,29 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+import TreeRoomBg from "../Images/Tree/TreeRoomBg"
+import TreeImage from "../Images/Tree/TreeImage"
+
 const TreeRoom: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to the Tree Room</h1>
-      <p>This is where the Christmas tree is located.</p>
+      <TreeImage css={treeCss} />
+      <TreeRoomBg css={treeBgCss} />
     </div>
   )
 }
 
 export default TreeRoom
+
+const treeCss = css`
+  position: absolute;
+  width: 90%;
+  height: auto;
+  right: -10%;
+  bottom: 8%;
+  z-index: 1;
+`
+
+const treeBgCss = css`
+  width: 100%;
+  height: 100%;
+`
