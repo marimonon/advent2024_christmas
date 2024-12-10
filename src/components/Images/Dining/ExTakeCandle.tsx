0d0403b1c92fc568d094ebda8934d7a7d0bbfa2d
marimonon <mappi-@hotmail.co.jp>
2024-12-10 21:10:15 +0900

    画像をコンポーネント化

---
+++ src/components/Images/Dining/ExTakeCandle.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+const ExTakeCandle: React.FC = () => {
+  return (
+    <svg
+      width="94"
+      height="443"
+      viewBox="0 0 94 443"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.0557 203.844C10.5898 205.944 9.85923 427.597 11.7771 429.515C13.695 431.433 26.5723 443.032 51.5962 442.393C76.7115 441.753 88.2187 432.712 90.1366 430.794C92.0545 428.876 91.4152 203.844 89.4973 202.565C87.5795 201.195 16.8915 200.556 13.0557 203.844Z"
+        fill="#EC7FAE"
+      />
+      <path
+        d="M52.2352 0C56.0709 0 61.2766 30.2296 68.3089 47.5819C75.3411 64.9343 101.735 108.041 91.4149 145.303C80.1815 186.127 16.5258 194.072 2.7353 140.828C-6.94548 103.566 25.8413 64.9343 34.2435 47.5819C39.9971 35.618 49.5866 0 52.2352 0Z"
+        fill="url(#paint0_radial_283_3840)"
+      />
+      <defs>
+        <radialGradient
+          id="paint0_radial_283_3840"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(51.5867 191.588) scale(177.432 177.432)"
+        >
+          <stop offset="0.17" stop-color="#FF9616" />
+          <stop offset="0.654" stop-color="#FFD429" />
+        </radialGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default ExTakeCandle
---
+++ src/components/Images/Tree/ExTreeSwitch.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+
+const ExTreeSwitch: React.FC = () => {
+  return (
+    <svg
+      width="75"
+      height="145"
+      viewBox="0 0 75 145"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        x="0.5"
+        y="0.5"
+        width="74"
+        height="144"
+        fill="url(#paint0_linear_283_3836)"
+        fill-opacity="0.5"
+        stroke="#D6D6D6"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_283_3836"
+          x1="37.5"
+          y1="0"
+          x2="37.5"
+          y2="145"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#666666" />
+          <stop offset="1" />
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default ExTreeSwitch
