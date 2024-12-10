c02fe000659d2943186bb327c3b7ba9d7d4be61e
marimonon <mappi-@hotmail.co.jp>
2024-12-10 16:41:18 +0900

    bgと全体のopacityのcss

--- src/components/Rooms/DiningRoom.tsx
+++ src/components/Rooms/DiningRoom.tsx
@@ -1,10 +1,25 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+import DiningBg from "../Images/Dining/DiningBg"
+import CakeImage from "../Images/Dining/CakeImage"
+import CakeFullImage from "../Images/Dining/CakeFullImage"
+
 const DiningRoom: React.FC = () => {
   return (
-    <div>
-      <h1>Dining Room</h1>
-      <p>Welcome to the dining room!</p>
+    <div css={offDiningCss}>
+      <CakeImage css={cakeCss} />
+      <CakeFullImage css={cakeFullCss} />
+      <DiningBg css={diningBgCss} />
     </div>
   )
 }
 
 export default DiningRoom
+const offDiningCss = css`
+  // opacity: 0.1;
+  pointer-events: none;
+`
+const diningBgCss = css`
+  width: 100%;
+  height: 100%;
+`
