5d58883314daf813149b817f4303f5c1861d312b
marimonon <mappi-@hotmail.co.jp>
2024-12-11 21:28:10 +0900

    上から下にグラデーション

--- src/components/Ending.tsx
+++ src/components/Ending.tsx
@@ -1,7 +1,24 @@
-import React from "react"
+/** @jsxImportSource @emotion/react */
+import { css, keyframes } from "@emotion/react"
+import EndTextImage from "./Images/Ending/EndTextImage"
+import SantasImage from "./Images/Ending/SantasImage"
+import NightImage from "./Images/Ending/NightImage"
 
 const Ending: React.FC = () => {
-  return <div>{/* Your content goes here */}</div>
+  return (
+    <div css={endBgCss}>
+      <EndTextImage />
+      <SantasImage />
+      <NightImage />
+    </div>
+  )
 }
 
 export default Ending
+
+const endBgCss = css`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to bottom, #045662, #fff);
+`
