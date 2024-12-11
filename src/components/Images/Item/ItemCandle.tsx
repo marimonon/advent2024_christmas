54abf02f8b8224f40bf4b951eed44e4bb06d8ba5
marimonon <mappi-@hotmail.co.jp>
2024-12-11 11:10:11 +0900

    画像コンポーネントの作成

---
+++ src/components/Images/Item/ItemCandle.tsx
@@ -0,0 +1,40 @@
+const ItemCandle: React.FC = () => {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_293_259)">
+        <path
+          d="M20.6838 22.1191C20.4162 22.347 20.337 46.3953 20.545 46.6033C20.7531 46.8114 22.1502 48.0698 24.8652 48.0005C27.5901 47.9311 28.8385 46.9501 29.0466 46.7421C29.2547 46.534 29.1854 22.1191 28.9773 21.9804C28.7692 21.8318 21.0999 21.7624 20.6838 22.1191Z"
+          fill="#EC7FAE"
+        />
+        <path
+          d="M24.9345 0C25.3507 0 25.9155 3.27975 26.6784 5.16239C27.4414 7.04503 30.305 11.7219 29.1853 15.7646C27.9666 20.1938 21.0603 21.0558 19.5641 15.2791C18.5138 11.2364 22.0709 7.04503 22.9825 5.16239C23.6068 3.86436 24.6472 0 24.9345 0Z"
+          fill="url(#paint0_radial_293_259)"
+        />
+      </g>
+      <defs>
+        <radialGradient
+          id="paint0_radial_293_259"
+          cx="0"
+          cy="0"
+          r="1"
+          gradientUnits="userSpaceOnUse"
+          gradientTransform="translate(24.8642 20.7863) scale(19.2505 19.2505)"
+        >
+          <stop offset="0.17" stop-color="#FF9616" />
+          <stop offset="0.654" stop-color="#FFD429" />
+        </radialGradient>
+        <clipPath id="clip0_293_259">
+          <rect width="48" height="48" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export default ItemCandle
