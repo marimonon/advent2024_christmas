6c87a7f702028ca3100377cb4cde0c75bafea906
marimonon <mappi-@hotmail.co.jp>
2024-12-11 21:57:45 +0900

    アニメーションの追加

--- src/components/Ending.tsx
+++ src/components/Ending.tsx
@@ -3,12 +3,16 @@ import { css, keyframes } from "@emotion/react"
 import EndTextImage from "./Images/Ending/EndTextImage"
 import SantasImage from "./Images/Ending/SantasImage"
 import NightImage from "./Images/Ending/NightImage"
+import LightImage from "./Images/Ending/LightImage"
+import WhiteLightImage from "./Images/Ending/WhiteLightImage"
 
 const Ending: React.FC = () => {
   return (
     <div css={endBgCss}>
       <EndTextImage css={endTextCss} />
       <SantasImage css={santasCss} />
+      <WhiteLightImage css={whiteCss} />
+      <LightImage css={lightCss} />
       <NightImage css={nightCss} />
     </div>
   )
@@ -46,12 +50,56 @@ const santasCss = css`
   left: -10%;
   width: 100%;
   height: auto;
+  z-index: 5;
+  animation: ${santasAnime} 5s infinite linear;
+`
+
+const whiteAnime = keyframes`
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`
+
+const whiteCss = css`
+  position: absolute;
+  top: 0%;
+  left: 0;
+  width: 100%;
+  height: auto;
   z-index: 4;
-  animation: ${santasAnime} 3s infinite;
+  animation: ${whiteAnime} 3s infinite;
+`
+const lightAnime = keyframes`
+  0% {
+    opacity: 0.5;
+  }
+  50% {
+    opacity: 0.1;
+  }
+  100% {
+    opacity: 0.5;
+  }
+`
+
+const lightCss = css`
+  position: absolute;
+  top: 0%;
+  left: 0;
+  width: 100%;
+  height: auto;
+  z-index: 3;
+  animation: ${lightAnime} 3s infinite;
 `
 
 const nightCss = css`
-  margin-top: 20%;
+  position: absolute;
+  top: 20%;
   left: 0;
   width: 100%;
   height: auto;
