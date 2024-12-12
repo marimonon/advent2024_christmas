6774ba9c6c631545c567fd03392150acb1095cbd
marimonon <mappi-@hotmail.co.jp>
2024-12-12 21:59:44 +0900

    optionのvalueの実装

--- src/components/Rooms/ExHomeWork.tsx
+++ src/components/Rooms/ExHomeWork.tsx
@@ -1,46 +1,48 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
 import Btn from "../Btn"
+import { useState } from "react"
 
 const ExHomeWork: React.FC = () => {
+  const [work, setWork] = useState(false)
   return (
     <div css={deskBgCss}>
       <div css={paperCss}>
         <h2>感想文</h2>
         <select css={paperLineCss}>
-          <option></option>
-          <option>アジャイルサムライの本は</option>
-          <option>コード・コンプリートの本は</option>
-          <option>マスタリングTCP/IPの本は</option>
-          <option>Good Code, Bad Codeの本は</option>
+          <option value="0"></option>
+          <option value="1">アジャイルサムライの本は</option>
+          <option value="2">コード・コンプリートの本は</option>
+          <option value="3">マスタリングTCP/IPの本は</option>
+          <option value="4">Good Code, Bad Codeの本は</option>
         </select>
         <select css={paperLineCss}>
-          <option></option>
-          <option>図表が多く、補足説明も多く、</option>
-          <option>イラストが面白く、</option>
-          <option>値段が高く極めて難解だが、</option>
-          <option>書く理由を詳しく述べてあり、</option>
+          <option value="0"></option>
+          <option value="1">図表が多く、補足説明も多く、</option>
+          <option value="2">イラストが面白く、</option>
+          <option value="3">値段が高く極めて難解だが、</option>
+          <option value="4">書く理由を詳しく述べてあり、</option>
         </select>
         <select css={paperLineCss}>
-          <option></option>
-          <option>合った事例を考えたり</option>
-          <option>身近で意外な発見もあり、</option>
-          <option>リアリティのある事例が多く</option>
-          <option>具体的に実践しやすい内容で</option>
+          <option value="0"></option>
+          <option value="1">合った事例を考えたり</option>
+          <option value="2">身近で意外な発見もあり、</option>
+          <option value="3">リアリティのある事例が多く</option>
+          <option value="4">具体的に実践しやすい内容で</option>
         </select>
         <select css={paperLineCss}>
-          <option></option>
-          <option>設計の細かい会話ができて</option>
-          <option>みんなでワイワイ話し合えて</option>
-          <option>すぐに実務に生かすことが出来</option>
-          <option>考えてなかったことを知れるので</option>
+          <option value="0"></option>
+          <option value="1">設計の細かい会話ができて</option>
+          <option value="2">みんなでワイワイ話し合えて</option>
+          <option value="3">すぐに実務に生かすことが出来</option>
+          <option value="4">考えてなかったことを知れるので</option>
         </select>
         <select css={paperLineCss}>
-          <option></option>
-          <option>すごく勉強になります</option>
-          <option>大変おすすめな本です。</option>
-          <option>とてもためになります。</option>
-          <option>とても楽しいです。</option>
+          <option value="0"></option>
+          <option value="1">すごく勉強になります</option>
+          <option value="2">大変おすすめな本です。</option>
+          <option value="3">とてもためになります。</option>
+          <option value="4">とても楽しいです。</option>
         </select>
       </div>
       <div css={workBtnCss}>
